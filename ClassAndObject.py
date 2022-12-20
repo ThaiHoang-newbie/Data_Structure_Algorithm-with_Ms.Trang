4966a0b8e21f87d4f7bdc02500ee7a3f85c81d5c
ThaiHoang-newbie <110578242+ThaiHoang-newbie@users.noreply.github.com>
2022-12-20 14:22:02 +0700

    Add files via upload

---
+++ ClassAndObject.py
@@ -0,0 +1,11 @@
+class xe:
+    def __init__(self, ten, mau, chat_lieu):
+        self.ten = ten
+        self.mau = mau
+        self.chat_lieu = chat_lieu
+    def chao():
+        return "Xin chào"
+name = xe("Xe A", "Đỏ", "Sắt")
+
+print(name.ten, name.mau)
+print(xe.chao())
