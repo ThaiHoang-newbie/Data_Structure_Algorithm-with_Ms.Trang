cb62ae72e8e083e010b2337238cb92c2484e5b6e
ThaiHoang-newbie <110578242+ThaiHoang-newbie@users.noreply.github.com>
2022-12-20 14:21:26 +0700

    Add files via upload

---
+++ Array2.py
@@ -0,0 +1,24 @@
+import array as arr
+numbers = arr.array('i',[])
+n = int(input("Nhập..."))
+
+
+
+
+"""for j in range(n):
+    numbers.append(int(input("Nhập số thứ %d: "%(j + 1))))
+numbers.append(3)
+m = int(input("Nhập số phần tử cần thêm: "))
+for j in range(m):
+    numbers.append(int(input("Nhập số thứ %d: "%(j + 1))))
+numbers.insert(3, 50)
+number = arr.array('i',[])
+x = int(input("Nhập..."))
+for j in range(x):
+    number.append(int(input("Nhập số thứ %d: "%(j + 1))))
+print(numbers)
+print(number)
+a = numbers + number
+vitri = int(input("Nhập vị trí cần xóa: "))
+print(a.pop(vitri))
+print(a)"""
